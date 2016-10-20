8e2a38006fded6fe5a3d993e5bc24585d5d6b6a9
Stuart Blackler (@Im5tu) <Im5tu@users.noreply.github.com>
2016-10-20 23:35:49 +0100

    Initial memory provider

---
+++ src/OpenMessage.Providers.Memory/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenMessage.Providers.Memory")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b9091f8b-bb27-4277-8cd4-8b2e4d80a4e4")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.Memory.Tests")]
